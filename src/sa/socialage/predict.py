Fix social age algorithm probability calculation

src/sa/socialage/predict.py
@@ -1,11 +1,12 @@
 import numpy as np
 from .models import Page
-
+from django.db.models import Sum
 # Precomputed prior distribution (P(agegroup))
 PRIOR = np.array([0.008, 0.02, 0.029, 0.023, 0.14, 0.23, 0.22, 0.23, 0.06, 0.04])
 # For debugging
 TEST_IDS = ["813286", "15846407"]
-MIN_TESTSTARS = 2
+# Minimum number of test stars required
+MIN_TESTSTARS = 1
 
 
 def predict(test_ids, network, debug=False):
@@ -22,12 +23,13 @@ def predict(test_ids, network, debug=False):
     """
     if debug:
         test_ids = TEST_IDS
+        network = 'tw'
     # Retrieve all pages
     pages = Page.objects.order_by('-total')
     if network == 'fb':
-        test_stars = pages.filter(fb_id__in=test_ids)
+        test_stars = pages.filter(fb_id__in=test_ids, total__gte=20)
     else:
-        test_stars = pages.filter(tw_id__in=test_ids)
+        test_stars = pages.filter(tw_id__in=test_ids, total__gte=00)
 
     # if the amount of test_stars is less than MIN_TESTSTARS,
     # return -1 and tell user that the pages they provided are
@@ -36,6 +38,9 @@ def predict(test_ids, network, debug=False):
         return -1
 
     prob = extract_prob(test_stars)
+    if debug:
+        print('Result of extract prob:')
+        print(prob)
     # Log the probabilities
     log_prob = np.log(prob)
     # Sum the log prob of all stars
@@ -49,21 +54,28 @@ def predict(test_ids, network, debug=False):
     # Normalize
     total = np.sum(age_prob)
     age_prob = age_prob / total
+    if debug:
+        print('Result of age prob:')
+        print(age_prob)
     age_groups = [10, 12.5, 14.5, 16.5, 21, 30, 40, 50, 60, 70]
-    return sum(map(lambda x,y:x*y,age_prob.tolist(), age_groups))
-
+    return sum(map(lambda x, y: x * y, age_prob.tolist(), age_groups))
 
 
 def extract_prob(test_stars):
     """
     :param test_stars: pages to be tested
-    :return: probabilities of different age groups given the pages P(page|agegroup) in a np.ndarray
+    :return: probabilities of following the page given different age groups P(page|agegroup) in a np.ndarray
     """
-
-
-    prob = np.zeros(shape=(1, 10), dtype=float)
+    prob = np.zeros(shape=(len(test_stars), 10), dtype=float)
     i = 0
-    total = 0
+    # Calculate the total number of number sample follows made by each age groups,
+    #   save them in a list.
+    f = lambda x: list(Page.objects.aggregate(Sum(x)).values())[0]
+    totals = [f('ageUnder12'), f('age12to13'), f('age14to15'), f('age16to17'), f('age18to24'),
+              f('age25to34'), f('age35to44'), f('age45to54'), f('age55to64'), f('ageAbove65')]
+
+
+    #
     for s in test_stars:
         prob[i][0] += s.ageUnder12
         prob[i][1] += s.age12to13
@@ -74,14 +86,10 @@ def extract_prob(test_stars):
         prob[i][6] += s.age35to44
         prob[i][7] += s.age45to54
         prob[i][8] += s.age55to64
-        #prob[i][9] += 0
-        # m-estimates, equivalent sample size m = 10, adding m virtual samples
-        # according to PRIOR
-        total += s.total
-
+        prob[i][9] += s.ageAbove65
+        i += 1
+    prob += 1
     for k in range(10):
-        prob[0][k] /= total
+        prob[:, k] /= (totals[k] + 10)
     return prob
-
-
 