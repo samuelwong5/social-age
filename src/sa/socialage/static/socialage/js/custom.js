9485603baf28220ba1e624b16bd04642e22255f4
Chun Chan <chun.k.chan13@imperial.ac.uk>
2016-01-13 02:11:43 +0000

    moving javascript on html to custom.js

--- src/sa/socialage/static/socialage/js/custom.js
+++ src/sa/socialage/static/socialage/js/custom.js
@@ -1,217 +1,121 @@
-
-
-
-/*=============================================================
-    Authour URI: www.binarycart.com
-    License: Commons Attribution 3.0
-
-    http://creativecommons.org/licenses/by/3.0/
-
-    100% To use For Personal And Commercial Use.
-    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
-   
-    ========================================================  */
-
-
-(function ($) {
-    "use strict";
-    var mainApp = {
-
-        main_fun: function () {
-            /*====================================
-            METIS MENU 
-            ======================================*/
-            $('#main-menu').metisMenu();
-
-            /*====================================
-              LOAD APPROPRIATE MENU BAR
-           ======================================*/
-            $(window).bind("load resize", function () {
-                if ($(this).width() < 768) {
-                    $('div.sidebar-collapse').addClass('collapse')
-                } else {
-                    $('div.sidebar-collapse').removeClass('collapse')
-                }
-            });
-
-            /*====================================
-            MORRIS BAR CHART
-         ======================================*/
-            Morris.Bar({
-                element: 'morris-bar-chart',
-                data: [{
-                    y: '2006',
-                    a: 100,
-                    b: 90
-                }, {
-                    y: '2007',
-                    a: 75,
-                    b: 65
-                }, {
-                    y: '2008',
-                    a: 50,
-                    b: 40
-                }, {
-                    y: '2009',
-                    a: 75,
-                    b: 65
-                }, {
-                    y: '2010',
-                    a: 50,
-                    b: 40
-                }, {
-                    y: '2011',
-                    a: 75,
-                    b: 65
-                }, {
-                    y: '2012',
-                    a: 100,
-                    b: 90
-                }],
-                xkey: 'y',
-                ykeys: ['a', 'b'],
-                labels: ['Series A', 'Series B'],
-                hideHover: 'auto',
-                resize: true
-            });
-
-            /*====================================
-          MORRIS DONUT CHART
-       ======================================*/
-            Morris.Donut({
-                element: 'morris-donut-chart',
-                data: [{
-                    label: "Download Sales",
-                    value: 12
-                }, {
-                    label: "In-Store Sales",
-                    value: 30
-                }, {
-                    label: "Mail-Order Sales",
-                    value: 20
-                }],
-                resize: true
-            });
-
-            /*====================================
-         MORRIS AREA CHART
-      ======================================*/
-
-            Morris.Area({
-                element: 'morris-area-chart',
-                data: [{
-                    period: '2010 Q1',
-                    iphone: 2666,
-                    ipad: null,
-                    itouch: 2647
-                }, {
-                    period: '2010 Q2',
-                    iphone: 2778,
-                    ipad: 2294,
-                    itouch: 2441
-                }, {
-                    period: '2010 Q3',
-                    iphone: 4912,
-                    ipad: 1969,
-                    itouch: 2501
-                }, {
-                    period: '2010 Q4',
-                    iphone: 3767,
-                    ipad: 3597,
-                    itouch: 5689
-                }, {
-                    period: '2011 Q1',
-                    iphone: 6810,
-                    ipad: 1914,
-                    itouch: 2293
-                }, {
-                    period: '2011 Q2',
-                    iphone: 5670,
-                    ipad: 4293,
-                    itouch: 1881
-                }, {
-                    period: '2011 Q3',
-                    iphone: 4820,
-                    ipad: 3795,
-                    itouch: 1588
-                }, {
-                    period: '2011 Q4',
-                    iphone: 15073,
-                    ipad: 5967,
-                    itouch: 5175
-                }, {
-                    period: '2012 Q1',
-                    iphone: 10687,
-                    ipad: 4460,
-                    itouch: 2028
-                }, {
-                    period: '2012 Q2',
-                    iphone: 8432,
-                    ipad: 5713,
-                    itouch: 1791
-                }],
-                xkey: 'period',
-                ykeys: ['iphone', 'ipad', 'itouch'],
-                labels: ['iPhone', 'iPad', 'iPod Touch'],
-                pointSize: 2,
-                hideHover: 'auto',
-                resize: true
-            });
-
-            /*====================================
-    MORRIS LINE CHART
- ======================================*/
-            Morris.Line({
-                element: 'morris-line-chart',
-                data: [{
-                    y: '2006',
-                    a: 100,
-                    b: 90
-                }, {
-                    y: '2007',
-                    a: 75,
-                    b: 65
-                }, {
-                    y: '2008',
-                    a: 50,
-                    b: 40
-                }, {
-                    y: '2009',
-                    a: 75,
-                    b: 65
-                }, {
-                    y: '2010',
-                    a: 50,
-                    b: 40
-                }, {
-                    y: '2011',
-                    a: 75,
-                    b: 65
-                }, {
-                    y: '2012',
-                    a: 100,
-                    b: 90
-                }],
-                xkey: 'y',
-                ykeys: ['a', 'b'],
-                labels: ['Series A', 'Series B'],
-                hideHover: 'auto',
-                resize: true
-            });
-           
-     
-        },
-
-        initialization: function () {
-            mainApp.main_fun();
+function drawStuff(id, options, json, bar) {
+        try {
+            var data = new google.visualization.arrayToDataTable(json);
+            if (bar) {
+                var chart = new google.visualization.BarChart(document.getElementById(id));
+            } else {
+                var chart = new google.visualization.ColumnChart(document.getElementById(id));
+            }
+        }
+        catch (err) {
 
         }
-
+        chart.draw(data, options);
     }
-    // Initializing ///
+    ;
 
     $(document).ready(function () {
-        mainApp.main_fun();
+        google.charts.load('current', {packages: ['corechart']});
+        $("#results").click(function () {
+            $.ajax({
+                url: "../results/",
+                success: function (result) {
+                    $("#content").html(result);
+                }
+            });
+        });
+        $("#recommend").click(function () {
+            $.ajax({
+                url: "../recommended/",
+                success: function (result) {
+                    $("#content").html(result);
+                }
+            });
+        });
+        $("#analysis").click(function () {
+            $.ajax({
+                url: "../analysis/",
+                success: function (result) {
+                    $("#content").html(result);
+                     $('#example').dataTable();
+                }
+            });
+        });
+        $("#friends").click(function () {
+            $.ajax({
+                url: "../friends/",
+                success: function (result) {
+                    $("#content").html(result);
+                }
+            });
+        });
+        $("#graphs").click(function () {
+            $.ajax({
+                url: "../graphs/",
+                success: function (result) {
+                    $("#content").html(result);
+                    $.ajax({
+                        url: "../graph_data/",
+                        success: function (result) {
+                            var options = {
+                                title: 'Average age of liked pages',
+                                width: 900,
+                                legend: {position: 'none'},
+                                chart: {
+                                    title: 'Liked Pages and Followed Users',
+                                    subtitle: 'average age'
+                                },
+                                axes: {
+                                    x: {
+                                        0: {side: 'top', label: 'Age'} // Top x-axis.
+                                    }
+                                },
+                                bar: {groupWidth: "90%"}
+                            };
+                            drawStuff("page-chart", options, result.page_age, true);
+                            options = {
+                                title: 'Distribution of other users\' social age with same biological age as you',
+                                width: 900,
+                                legend: {position: 'none'},
+                                chart: {
+                                    title: 'Distribution of Social Age',
+                                    subtitle: 'Biological Age = 24'
+                                },
+                                axes: {
+                                    x: {
+                                        0: {side: 'top', label: 'Social Age'} // Top x-axis.
+                                    },
+                                    y: {
+                                        0: {label: 'Frequency'} // Top x-axis.
+                                    }
+                                },
+                                bar: {groupWidth: "90%"}
+                            };
+                            drawStuff("bio-age", options, result.bio_dist, false);
+
+                            options = {
+                                title: 'Distribution of other users\' biological Age with same social age as you',
+                                width: 900,
+                                legend: {position: 'none'},
+                                chart: {
+                                    title: 'Distribution of Biological Age',
+                                    subtitle: 'Social Age = ' + result.soc_age.toString()
+                                },
+                                axes: {
+                                    x: {
+                                        0: {side: 'top', label: 'Biological Age'} // Top x-axis.
+                                    },
+                                    y: {
+                                        0: {label: 'Frequency'} // Top x-axis.
+                                    }
+                                },
+                                bar: {groupWidth: "90%"}
+                            };
+                            drawStuff("soc-age", options, result.soc_dist, false);
+                        }
+                    });
+                }
+            });
+        });
     });
 
-}(jQuery));
