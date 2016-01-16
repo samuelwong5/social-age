b2e66d0e02813faf269bcef526aca058c73445c1
Michael Cheung <Michael@Michaels-MBP-3.home>
2016-01-16 14:15:59 +0000

    initial iOs commit

---
+++ iOS/social-ageIOS/SocialApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Michael Cheung on 1/16/16.
+//  Copyright © 2016 i-connect. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
